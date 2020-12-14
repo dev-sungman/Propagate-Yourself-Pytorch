c2eb538083481e482b2394400ad069f13a7518b4
root <root@csi-cluster-vgpu28.dakao.io>
2020-12-14 21:50:18 +0900

    update downstream task

--- downstream/convert-pretrain-to-detectron2.py
+++ downstream/convert-pretrain-to-detectron2.py
@@ -13,8 +13,29 @@ if __name__ == "__main__":
 
     newmodel = {}
     for k, v in obj.items():
-        if not k.startswith("module.encoder_q."):
+        if not k.startswith("module.base_encoder."):
             continue
+        print(k)
+        old_k = k
+        k = k.replace("module.base_encoder.", "")
+        if "layer" not in k:
+            k = "stem." + k
+        for t in [1, 2, 3, 4]:
+            k = k.replace("layer{}".format(t), "res{}".format(t+1))
+
+        for t in [1, 2, 3]:
+            k = k.replace("bn{}".format(t), "conv{}.norm".format(t))
+
+        k = k.replace("downsample.0", "shortcut")
+        k = k.replace("downsample.1", "shortcut.norm")
+        print(old_k, "->", k)
+        newmodel[k] = v.numpy()
+    res = {"model": newmodel, "__author__": "Pixpro", "matching_heuristics": True}
+
+    with open(sys.argv[2], 'wb') as f:
+        pkl.dump(res, f)
+
+        '''
         old_k = k
         k = k.replace("module.encoder_q.", "")
         if "layer" not in k:
@@ -27,8 +48,10 @@ if __name__ == "__main__":
         k = k.replace("downsample.1", "shortcut.norm")
         print(old_k, "->", k)
         newmodel[k] = v.numpy()
-
+        '''
+    '''    
     res = {"model": newmodel, "__author__": "MOCO", "matching_heuristics": True}
 
     with open(sys.argv[2], "wb") as f:
         pkl.dump(res, f)
+    '''
